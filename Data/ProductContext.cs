be8e18e6cf371bc676582a82913275d4ee11871b
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-24 06:33:39 +0300

    restructure

---
+++ Data/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using SoftwareTracker.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftwareTracker.Data
+{
+  public class ProductContext : DbContext
+  {
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<ProductVersion> ProductVersions { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseNpgsql("<connection string>");
+    }
+  }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftwareTracker.Model
+{
+  public class Product
+  {
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string Developer { get; set; } = null!;
+    public string SourceUrl { get; set; } = null!;
+
+    public DateTime createdAt { get; set; }
+
+    public ICollection<ProductVersion> Versions { get; set; } = [];
+  }
+}
