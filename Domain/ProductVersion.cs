525ec2df1074ff321d5ab63b98a716f9728dccf7
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-24 05:53:32 +0300

    Domain: intial implementation

---
+++ Domain/ProductVersion.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftwareTracker.Domain
+{
+  public class ProductVersion
+  {
+    public Guid Id { get; set; }
+
+    public Guid ProductId { get; set; }
+    public string Version { get; set; } = null!;
+    public string ReleaseDate { get; set; } = null!;
+    public string SourceUrl { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+
+    public Product Product { get; set; } = null!;
+  }
+}
