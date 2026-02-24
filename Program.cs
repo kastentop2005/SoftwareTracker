6e066dd9c51f5fcba893cd6b58e4b1efee0e3b78
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-24 05:53:57 +0300

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,23 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using SoftwareTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Define secret connection string
+builder.Services.AddDbContext<ProductContext>(options =>
+  options.UseNpgsql(builder.Configuration.GetConnectionString("SoftwareTracker")));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
