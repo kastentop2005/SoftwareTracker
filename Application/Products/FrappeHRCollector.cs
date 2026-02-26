9dc0744729f1faf3071a7d2526feace18715f533
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-26 06:27:08 +0300

    Products: implement Frappe HR Collector

---
+++ Application/Products/FrappeHRCollector.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftwareTracker.Application.Products
+{
+  public class FrappeHRCollector : GitHubCollector
+  {
+    public override string ProductName => "hrms";
+    public override string SourceUrl => "https://github.com/frappe/hrms";
+    protected override string RepoOwner => "frappe";
+    protected override string RepoName => "hrms";
+
+    protected override string ApiEndpoint => "tags";
+  }
+}
