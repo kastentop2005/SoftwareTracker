d7e28ba41808e180e219a7a9d082ad5287e2890f
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-26 06:25:30 +0300

    Products: Implement AFFiNE collector

---
+++ Application/Products/AFFiNECollector.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftwareTracker.Application.Products
+{
+  public class AFFiNECollector : GitHubCollector
+  {
+    public override string ProductName => "AFFiNE";
+    public override string SourceUrl => "https://github.com/toeverything/AFFiNE";
+    protected override string RepoOwner => "toeverything";
+    protected override string RepoName => "AFFiNE";
+
+    protected override string ApiEndpoint => "releases";
+  }
+}
