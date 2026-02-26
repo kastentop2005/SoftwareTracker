c9dd47cce8908bc0842f0a0e398866be05b31465
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-26 06:29:02 +0300

    Products: implement Zimbra collector

---
+++ Application/Products/ZimbraCollector.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftwareTracker.Application.Products
+{
+  public class ZimbraCollector : GitHubCollector
+  {
+    public override string ProductName => "Zimbra";
+    public override string SourceUrl => "https://github.com/Zimbra/zm-mailbox";
+    protected override string RepoOwner => "Zimbra";
+    protected override string RepoName => "zm-mailbox";
+
+    protected override string ApiEndpoint => "tags";
+  }
+}
