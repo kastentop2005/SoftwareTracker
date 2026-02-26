e981222091a6019905fe3f4f63e2e5b24f881e44
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-26 06:27:58 +0300

    Products: implement OpenEMR Collector

---
+++ Application/Products/OpenEMRCollector.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftwareTracker.Application.Products
+{
+  public class OpenEMRCollector : GitHubCollector
+  {
+    public override string ProductName => "OpenEMR";
+    public override string SourceUrl => "https://github.com/openemr/openemr";
+    protected override string RepoOwner => "openemr";
+    protected override string RepoName => "openemr";
+
+    protected override string ApiEndpoint => "tags";
+  }
+}
