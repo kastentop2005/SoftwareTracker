7a9d3226e6ed5d67558103ebdc30e62ed307fb09
Lipinskiy Konstantin Stanislavovich <kastentop2005@gmail.com>
2026-02-27 01:35:28 +0300

    Program: define connection string and register collectors

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SoftwareTracker.Application;
+using SoftwareTracker.Application.Products;
 using SoftwareTracker.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<ProductContext>();
+builder.Services.AddDbContext<ProductContext>(options => 
+  options.UseNpgsql(builder.Configuration.GetConnectionString("SoftwareTracker")));
+
+// Register collcetors
+builder.Services.AddScoped<IVersionCollector, AFFiNECollector>();
+builder.Services.AddScoped<IVersionCollector, ZimbraCollector>();
+builder.Services.AddScoped<IVersionCollector, QBitCollector>();
+builder.Services.AddScoped<IVersionCollector, OpenEMRCollector>();
+builder.Services.AddScoped<IVersionCollector, FrappeHRCollector>();
+
+// Register the sync service
+builder.Services.AddScoped<VersionSyncService>();
 
 var app = builder.Build();
 
